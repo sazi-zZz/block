f759732b072a04542318725d79b71acb760b1dae
lesgo-Beyond <sazedurrahman707@gmail.com>
2026-03-21 04:32:13 +0600

    updated config for localusage also

--- config/db.php
+++ config/db.php
@@ -5,6 +5,12 @@ define('DB_USER', 'DB_USER_PLACEHOLDER');
 define('DB_PASS', 'DB_PASS_PLACEHOLDER');
 define('DB_NAME', 'DB_NAME_PLACEHOLDER');
 
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'block_app');
+
+
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
